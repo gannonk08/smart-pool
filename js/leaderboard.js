93ba8cfb329fe294036fa9560f1c6d8cda1717b7
Kristjan Gannon <gannonk08@gmail.com>
2016-08-11 14:10:41 -0600

    Prepresentation verision

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -1,19 +1,21 @@
     console.log('leaderboard.js sanity check!');
 $(document).on('ready', function() {
     var mastersLeaderboardUrl = 'https://galvanize-cors-proxy.herokuapp.com/http://api.sportradar.us/golf-t2/leaderboard/pga/2016/tournaments/b95ab96b-9a0b-4309-880a-ad063cb163ea/leaderboard.json=?api_key=f6dhcjtrn5xbsm3fkjg439ud';
-    var leaderboard 
+    var leaderboard
     $.ajax({
         url: mastersLeaderboardUrl,
         method: 'GET',
         dataType: 'json'
-    }).done(function(data) {
+    }).then(function(data) {
         var leaderboardArr = data['leaderboard']
         appendLeaderboard(leaderboardArr)
 
         console.log(data['leaderboard'])
         console.log(data['leaderboard'][0]['position'])
             // $('#leaderboardWell').text(data);
-    });
+    }).then(function(data) {
+        $('#leaderTable').dataTable();
+      })
 
 });
 
