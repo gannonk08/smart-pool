9848fbc9959a03d7832440d100296288b02daa0f
Kristjan Gannon <gannonk08@gmail.com>
2016-08-11 11:58:48 -0600

    working sortable standings

--- src/js/scoreCalc.js
+++ src/js/scoreCalc.js
@@ -1,20 +1,13 @@
 console.log('scoreCalc.js sanity check!');
 $(document).ready(function() {
+  var formattedEntryArray = []
+  var scoreArray = [];
+
+
+
+
   //appends formatted entries to the standings div in
-  userPicksObjTest = {
-      entryName: "Kristjan Gannon",
-      tier1: "# 1 . Day, Jason",
-      tier2: "# 19 . Matsuyama, Hideki",
-      tier3: "# 37 . Furyk, Jim",
-      tier4: "# 56 . Streb, Robert"
-  }
-  userPicksObjTest2 = {
-      entryName: "Kristjan Gannon",
-      tier1: "# 1 . Day, Jason",
-      tier2: "# 19 . Matsuyama, Hideki",
-      tier3: "# 37 . Grillo, Emiliano",
-      tier4: "# 56 . English, Harris"
-  }
+
   var mastersLeaderboardUrl = 'https://galvanize-cors-proxy.herokuapp.com/http://api.sportradar.us/golf-t2/leaderboard/pga/2016/tournaments/b95ab96b-9a0b-4309-880a-ad063cb163ea/leaderboard.json=?api_key=f6dhcjtrn5xbsm3fkjg439ud';
   $.ajax({
     url: mastersLeaderboardUrl,
@@ -24,27 +17,38 @@ $(document).ready(function() {
     var leaderboardArr = data['leaderboard']
     return leaderboardArr
   }).then(function(leaderboardArr) {
-    console.log(leaderboardArr);
-    var scoreArray = [];
-    var formattedEntryArray = formatEntry(userPicksObjTest);
-    console.log(formattedEntryArray);
-    leaderboardArr.forEach(function(key) {
-      var golferNameLeaders = key['last_name'] + ", " + key['first_name'];
-      console.log(typeof golferNameLeaders);
-      for (var index in formattedEntryArray) {
-        if (golferNameLeaders === formattedEntryArray[index]) {
-          console.log(golferNameLeaders);
-          console.log(formattedEntryArray[index]);
-          scoreArray.push(key['score']);
-        }
+    let unformattedEntryArray = testEntryArray;
+    unformattedEntryArray.forEach(function(unformattedEntry){
+      formattedEntryArray.push(formatEntry(unformattedEntry))
+    });
+    // console.log(leaderboardArr);
+    // console.log(formattedEntryArray);
+  for (var i = 0; i < formattedEntryArray.length; i++) {
+    var entryScoresArray =[];
+    for (var entry in formattedEntryArray[i]) {
+    leaderboardArr.forEach(function(player) {
+      var golferNameLeaders = player['last_name'] + ", " + player['first_name'];
+          if (golferNameLeaders === formattedEntryArray[i][entry]) {
+            // console.log(formattedEntryArray[i][entry]);
+            entryScoresArray.push(player['score']);
+          }
+        })
       }
-    })
+      scoreArray.push(entryScoresArray);
+    }
+    console.log(scoreArray);
     return scoreArray;
   }).then(function(scoreArray) {
-    console.log(scoreArray);
-    var formattedEntryArray = formatEntry(userPicksObjTest);
-    appendStandings(formattedEntryArray, scoreArray);
-  })
+    // console.log(scoreArray);
+    console.log(formattedEntryArray);
+    i=0;
+    formattedEntryArray.forEach(function(formattedEntryObj){
+        appendStandings(formattedEntryObj, scoreArray[i]);
+        i++
+      })
+  }).then(function(data) {
+      $('#scoringTable').dataTable();
+    })
 })
 function appendStandings(formattedEntryObj, scoreArray) {
     var liveTier1Score = scoreArray[0];
@@ -55,13 +59,20 @@ function appendStandings(formattedEntryObj, scoreArray) {
     var entryName = ("<td>" + formattedEntryObj["entryNameFormatted"] + "</td>");
     var aggScore = ("<td>" + (liveTier1Score + liveTier2Score + liveTier3Score + liveTier4Score) + "</td>");
     var tier1 = ("<td>" + formattedEntryObj["tier1Formatted"] + "</td>")
+    console.log(tier1);
     var tier1Score = "<td>" + liveTier1Score + "</td>";
     var tier2 = ("<td>" + formattedEntryObj["tier2Formatted"] + "</td>");
+    console.log(tier2);
+
     var tier2Score = "<td>" + liveTier2Score + "</td>";
-    var tier3 = ("<td>" + formattedEntryObj["tier2Formatted"] + "</td>");
+    var tier3 = ("<td>" + formattedEntryObj["tier3Formatted"] + "</td>");
+    console.log(tier3);
+
     var tier3Score = "<td>" + liveTier3Score + "</td>";
     var tier4 = ("<td>" + formattedEntryObj["tier4Formatted"] + "</td>");
+    console.log(tier4);
+
     var tier4Score = "<td>" + liveTier4Score + "</td>";
 
-    $('#scoringTable').append("<tr>" + position + entryName + aggScore + tier1 + tier1Score + tier2 + tier2Score + tier3 + tier3Score + tier4 + tier4Score + "</tr>");
+    $('#scoringTable').append("<tr>" + entryName + aggScore + tier1 + tier1Score + tier2 + tier2Score + tier3 + tier3Score + tier4 + tier4Score + "</tr>");
 }
--- src/js/utilities.js
+++ src/js/utilities.js
@@ -1,32 +1,39 @@
 //format submission for comparison
 //format of submission initially
-userPicksObjTest = {
-    entryName: "Kristjan Gannon",
-    tier1: "# 1 . Day, Jason",
-    tier2: "# 19 . Matsuyama, Hideki",
-    tier3: "# 37 . Grillo, Emiliano",
-    tier4: "# 56 . English, Harris"
-}
 
-var formattedEntryObj = {
-  entryNameFormatted: "",
-  tier1Formatted: "",
-  tier2Formatted: "",
-  tier3Formatted: "",
-  tier4Formatted: ""
-}
+
+
+
 //Removes "#1 . " from entry obj
-function formatEntry (entryObj) {
-  formattedEntryObj.entryNameFormatted = entryObj["entryName"];
-  formattedEntryObj.tier1Formatted = removeNumbering(entryObj["tier1"])
-  formattedEntryObj.tier2Formatted = removeNumbering(entryObj["tier2"])
-  formattedEntryObj.tier3Formatted = removeNumbering(entryObj["tier3"])
-  formattedEntryObj.tier4Formatted = removeNumbering(entryObj["tier4"])
-  return formattedEntryObj
-
+function formatEntry(entryObj) {
+    var formattedEntryObj = {};
+    formattedEntryObj.entryNameFormatted = entryObj["entryName"];
+    formattedEntryObj.tier1Formatted = removeNumbering(entryObj["tier1"])
+    formattedEntryObj.tier2Formatted = removeNumbering(entryObj["tier2"])
+    formattedEntryObj.tier3Formatted = removeNumbering(entryObj["tier3"])
+    formattedEntryObj.tier4Formatted = removeNumbering(entryObj["tier4"])
+    return formattedEntryObj
 }
 
 function removeNumbering(tierString) {
-  tierString = tierString.split(". ").pop();
-  return tierString;
+    tierString = tierString.split(". ").pop();
+    return tierString;
 }
+
+
+var testEntryArray = [
+  testEntryObj1 = {
+    entryName: "Kristjan Test",
+    tier1: "# 1 . Day, Jason",
+    tier2: "# 21 . Johnson, Zach",
+    tier3: "# 37 . Westwood, Lee",
+    tier4: "# 62 . Simpson, Webb"
+  },
+  testEntryObj2 = {
+  entryName: "Jabroni Test",
+  tier1: "# 3 . Spieth, Jordan",
+  tier2: "# 21 . Koepka, Brooks",
+  tier3: "# 37 . Willett, Danny",
+  tier4: "# 62 . Horschel, Billy"
+  }
+]
